cfcdd681ac958ccd3b38e7e979d1e7f85a0a4422
ASUS <azharasykari@gmail.com>
2024-09-03 17:44:51 +0700

    updating about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,19 +1,25 @@
 "use client"
-import React from 'react'
-import { 
+import React, { useState } from 'react'
+import {
   Box,
   Heading,
   Text,
   Flex,
   Image,
   Button,
-  IconButton
+  IconButton,
+  Tab,
+  TabPanel,
+  Tabs
+  
 } from '@chakra-ui/react'
 import Navbar from '../ui/navbar'
 import { MemberList } from '../data/memberlist'
 import PictSlider from '../components/pictslider'
+import { FaLightbulb, FaLeaf, FaCheckCircle, FaRocket, FaUserTie, FaShieldAlt, FaHandshake, FaEye, FaRedo } from 'react-icons/fa';
 
 const About: React.FC = () => {
+  const [ activeTab, setActiveTab ] = useState('vision')
   return (
     <Box w="100%" h="300vh" bg="black">
       <Navbar />
@@ -22,7 +28,7 @@ const About: React.FC = () => {
           <Heading fontSize="80px" fontFamily="Inter, sans-serif"
             zIndex="1"
             bgGradient="linear(to-r, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.7))"
-            bgClip="text" 
+            bgClip="text"
           >Building community can be tough, but you don’t need to do it alone</Heading>
           <Text fontSize="px" pt="4%" w="70%" m="auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla imperdiet magna at orci tristique sagittis. Morbi justo sem, iaculis ac efficitur gravida, mollis a massa. Nunc consectetur venenatis augue, id sagittis massa convallis in. Phasellus tempor ipsum fringilla faucibus</Text>
         </Box>
@@ -33,7 +39,7 @@ const About: React.FC = () => {
               <Heading fontSize="48px" fontFamily="Inter, sans-serif"
                 zIndex="1"
                 bgGradient="linear(to-r, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.7))"
-                bgClip="text" 
+                bgClip="text"
               >Noble Life Mission adalah sebuah komunitas yang lahir dari ikatan persahabatan</Heading>
               <Text pt="6%" opacity="0.8" w="650px
               ">Di sini, setiap individu dihargai dan diangkat, bukan hanya sebagai teman, tetapi sebagai bagian dari keluarga besar yang saling menginspirasi dan memperkuat. Noble Life Mission berkomitmen untuk membangun sebuah ekosistem di mana nilai-nilai kebajikan, empati, dan kolaborasi menjadi fondasi dari setiap interaksi. </Text>
@@ -48,15 +54,203 @@ const About: React.FC = () => {
             color="white"
             bottom="-5%"
             fontSize="130px"
-            right="10%"
-          >Vision</Heading>
+            right="7%"
+          >Vision & Mission</Heading>
         </Box>
 
-        <Box bg="white" w="100%" h="100vh" borderRadius="80px 0px 0px 0px">
-          <Box>
+        <Box bg="white" w="100%" h="117vh" borderRadius="80px 0px 0px 0px" overflow="hidden">
+          <Box w="100%" h="auto" m="auto"
 
+          >
+            <Box color="white" display="flex"
+              justifyContent="center" gap="20px"
+              mt='12%'
+             
+            >
+              <Box display="flex" bg="beige" w="20%"
+                borderRadius="20px"
+                bgColor="black"
+              >
+                <Button
+                  p="28px"
+                  px="6%"
+                  w="50%"
+                  boxShadow="md"
+                  fontSize="20px"
+                  bg={activeTab === 'Vision' ? 'orange' : 'black'}
+                  color="white"
+                  _hover="none"
+                  textShadow="2px 2px 4px rgba(0, 0, 0, 0.5)"
+                  onClick={() => setActiveTab('Vision')}
+                >
+                  Vision
+                </Button>
+                <Button
+                  p="28px"
+                  px="6%"
+                  w="50%"
+                  boxShadow="md"
+                  fontSize="20px"
+                  bg={activeTab === 'Mission' ? 'orange' : 'black'}
+                  color="white"
+                  textShadow="2px 2px 4px rgba(0, 0, 0, 0.5)"
+                  _hover="none"
+                  onClick={() => setActiveTab('Mission')}
+                >
+                  Mission
+                </Button>
+              </Box>
+            </Box>
+            <Heading
+              fontFamily="Inter, sans-serif"
+              textAlign="center"
+              w="70%"
+              m="auto"
+              pt="4%"
+            >
+              {activeTab === 'Vision' && (
+                <>
+                  Menjadi platform kreatif terdepan yang menghubungkan dan memberdayakan para kreator untuk menghasilkan karya-karya unggulan yang dikenal luas, berdampak positif pada masyarakat, serta mendorong kolaborasi lintas sektor guna menciptakan perubahan yang berkelanjutan dan inovatif.
+                  <Box display="flex" flexWrap="wrap"
+                    justifyContent="center"
+                    mt="6%"
+                    bg="#eaeaea"
+                    borderRadius="10px"
+                    h="auto"
+                    w="100%"
+                    p="15px"
+                    gap="10px"
+                    mb="0"
+                  >
+                    {[
+                      { label: "Creativity", icon: FaLightbulb },
+                      { label: "Sustainability", icon: FaLeaf },
+                      { label: "Quality", icon: FaCheckCircle },
+                      { label: "Innovation", icon: FaRocket },
+                      { label: "Client-Centric", icon: FaUserTie },
+                      { label: "Integrity", icon: FaShieldAlt },
+                      { label: "Collaboration", icon: FaHandshake },
+                      { label: "Attention to Detail", icon: FaEye },
+                      { label: "Flexibility", icon: FaRedo }
+                    ].map((value, index) => (
+                      <Box key={index} mb="0" w="370px">
+                        <Box
+                          bg="black"
+                          borderRadius="8px"
+                          color="white"
+                          boxShadow="md"
+                          p="4%"
+                          display="flex"
+                          alignItems="center"
+                        >
+                          <Box mr="12px" fontSize="24px" color="orange">
+                            <value.icon /> {/* Menampilkan ikon */}
+                          </Box>
+                          <Text fontSize="18px" textAlign="left">{value.label}</Text>
+                        </Box>
+                      </Box>
+                    ))}
+                  </Box>
+                </>
+              )}
+            </Heading>
+
+            <Heading
+              fontFamily="Inter, sans-serif"
+              textAlign="left"
+              w="70%"
+              m="auto"
+              pt="4%"
+            >
+              {activeTab === 'Mission' && (
+                <>
+                  <Box textAlign="center" mb="0">
+                    <Box mb="4" display="flex" alignItems="center" justifyContent="center">
+                      <Box
+                        bg="orange"
+                        color="white"
+                        fontSize="24px"
+                        fontWeight="bold"
+                        w="70px"
+                        h="40px"
+                        borderRadius="50%"
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        mr="25px"
+                      >
+                        1
+                      </Box>
+                      <Box textAlign="left">
+                        <strong>Mengangkat Karya</strong>
+                        <br />
+                        Mengembangkan dan memperkenalkan karya-karya inovatif yang mencerminkan nilai-nilai budaya dan kreativitas.
+                      </Box>
+                    </Box>
+
+                    <Box mb="4" display="flex" alignItems="center" justifyContent="center">
+                      <Box
+                        bg="orange"
+                        color="white"
+                        fontSize="24px"
+                        fontWeight="bold"
+                        w="70px"
+                        h="40px"
+                        borderRadius="50%"
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        mr="25px"
+                      >
+                        2
+                      </Box>
+                      <Box textAlign="left">
+                        <strong>Memberi Manfaat</strong>
+                        <br />
+                        Menyediakan solusi dan kontribusi nyata yang berdampak positif pada masyarakat luas melalui inisiatif dan proyek sosial.
+                      </Box>
+                    </Box>
+
+                    <Box mb="4" display="flex" alignItems="center" justifyContent="center">
+                      <Box
+                        bg="orange"
+                        color="white"
+                        fontSize="24px"
+                        fontWeight="bold"
+                        w="90px"
+                        h="40px"
+                        borderRadius="50%"
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        mr="25px"
+                      >
+                        3
+                      </Box>
+                      <Box textAlign="left">
+                        <strong>Membangun Koneksi</strong>
+                        <br />
+                        Mendorong terciptanya jaringan kolaboratif yang kuat antara kreator, profesional, dan masyarakat untuk menghasilkan karya-karya yang relevan dan bermanfaat.
+                      </Box>
+                    </Box>
+                  </Box>
+
+                </>
+              )}
+            </Heading>
           </Box>
         </Box>
+        <Heading fontSize="100px" color="orange" pos="absolute"
+          bottom="0"
+          mx="10%"
+          my="-6%"
+        >OUR FOUNDER</Heading>
+      </Box>
+
+      <Box w="100%" h="100vh"
+        bg="orange"
+      >
+
       </Box>
     </Box>
   )
